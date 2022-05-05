6b0cba813de23b585499a7e0f350feec17e3fed5
ceribe <ceribekagami@gmail.com>
2022-05-05 21:40:17 +0200

    Refactor

--- memory.ml
+++ memory.ml
@@ -1,17 +1,16 @@
-open Bogue;;
+open Bogue
 module W = Widget
 module L = Layout
 module T = Trigger
 
-(** Utils functions *)
+(** Utility functions *)
 
 (* Returns a copy of list "d" but, order of its elements is random *)
-let shuffle d = begin
+let shuffle d = 
     Random.self_init ();
     let nd = List.map (fun c -> (Random.bits (), c)) d in
     let sond = List.sort compare nd in
     List.map snd sond
- end
 
 (* Chunks given "xs" list. Each chunk's lenght will be equal to "size" 
   chunk_list [1;2;3;4;5;6;7;8;9] 3 => [[1;2;3];[4;5;6];[7;8;9]] *)
@@ -24,52 +23,48 @@ let chunk_list xs size =
   in
   rs @ [r]
 
-(** Program logic *)
+(** Constants initalization *)
 
 (* List's lenght is 13 so after doubling each element and removing one it will be possible to place them in a 5x5 grid *)
-let tiles_names = ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j";"k";"l";"m"];;
+let tiles_names = ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j";"k";"l";"m"]
 
 (* Double the occurence of each element so there will be a pair of each letter *)
-let tiles_names_doubled = tiles_names @ tiles_names;;
+let tiles_names_doubled = tiles_names @ tiles_names
 
 (* Shuffle the list so every time the app is launched placement of pairs will be different *)
-let shuffled_names = shuffle tiles_names_doubled;;
+let shuffled_names = shuffle tiles_names_doubled
 
-(* Remove one value from shuffled_names so there is 25 elements total (5x5 grid) *)
-let names = List.tl shuffled_names;;
+(* Remove one value from shuffled_names so there are 25 elements total (5x5 grid) *)
+let names = List.tl shuffled_names
 
 let button_bg_color = (Draw.(opaque(find_color "#00FFFF")))
 
 (** Variables representing game state *)
 
-let found_pairs = ref 0;;
-let prev_button = ref (W.button "");;
-let prev_button_2 = ref (W.button "");;
-let prev_value = ref "";;
-let already_guessed_count = ref 0;;
-let guess_count = ref 0;;
-let end_label_text = ref "";;
-
+let found_pairs = ref 0
+let prev_button = ref (W.button "")
+let prev_button_2 = ref (W.button "")
+let prev_value = ref ""
+let already_guessed_count = ref 0
+let total_guess_count = ref 0
+let end_label_text = ref ""
 
 (** Functions used to respond to user input *)
 
 let end_game layout =
-   end_label_text := "Game over.";
-   Popup.info ~w:100 ~h:70 "You won!" layout;;
-
+   end_label_text := "Game over";
+   Popup.info ~w:60 ~h:40 "You won!" layout
+;;
 
 (* Checks if user guessed correctly and if so marks tiles with X *)
 let process_guess button name_label layout = 
   if (W.get_text name_label) = !prev_value then begin
+    already_guessed_count := 0;
     W.set_text button "X";
     W.set_text !prev_button "X";
     found_pairs := !found_pairs + 1;
-    if (!found_pairs = 12) then begin end_game layout;
-    end;
-
-    already_guessed_count := 0
+    if !found_pairs = 12 then end_game layout
   end
-;;
 
 (* Resets previous guesses by hiding them *)
 (* The paramter is not needed, but when there wasn't one this function wasn't called properly *)
@@ -77,10 +72,9 @@ let reset_prev_guesses _ =
   already_guessed_count := 1;
   W.set_text !prev_button_2 "?";
   W.set_text !prev_button "?"
-;;
 
 let process_tile_click button name_label layout = 
-  guess_count := !guess_count + 1;
+  total_guess_count := !total_guess_count + 1;
   already_guessed_count := !already_guessed_count + 1;
   if !already_guessed_count == 2 then
     process_guess button name_label layout;
@@ -90,17 +84,17 @@ let process_tile_click button name_label layout =
   prev_button_2 := !prev_button;
   prev_button := button;
   prev_value := W.get_text name_label;
+
+  (* If guess was correct do not change it to a letter *)
   if (W.get_text button) <> "X" then
     W.set_text button (W.get_text name_label)
-;;
 
 (** UI definition  *)
 
 let main () =
   let moves_label = W.label "Moves: 000" in
-  let end_label = W.label "                        " in
+  let end_label = W.label "                       " in
 
-  
   (* A flat list of buttons with labels *)
   let buttons = List.init 25 (fun _ -> W.button ~bg_off:(Solid button_bg_color) "?") in  
 
@@ -113,28 +107,27 @@ let main () =
   (* Create a list of columns. Each column has 5 buttons *)
   let columns = List.map (fun l -> L.tower_of_w ~w:30 l) (chunk_list buttons 5) in
 
-  let flat_layout_with_text = L.flat_of_w [moves_label] in
-  let squares_grid = L.flat ~sep:(-9) columns in
-  let end_layout= L.flat_of_w [end_label] in
+  let top_layout = L.flat_of_w [moves_label] in
+  let squares_grid_layout = L.flat ~sep:(-9) columns in
+  let bottom_layout = L.flat_of_w [end_label] in
 
-
-  (* Function run on each frame before displaying UI *)
+  (* Runs on each frame before displaying UI *)
   let before_display () =
-    W.set_text moves_label ("Moves: " ^ string_of_int !guess_count);
-    W.set_text end_label !end_label_text in
+    W.set_text moves_label ("Moves: " ^ string_of_int !total_guess_count);
+    W.set_text end_label !end_label_text 
+  in
 
-  let layout = L.tower [flat_layout_with_text;squares_grid;end_layout] in
+  let layout = L.tower [top_layout;squares_grid_layout;bottom_layout] in
 
   let on_tile_click button name_label _ = 
     if (W.get_text button) = "?" then (process_tile_click button name_label layout)
-    else ()
   in
 
-  (* Binds all buttons and their names with tile_click function *)
+  (* Binds all buttons and their names with on_tile_click function *)
   let connections = List.map2 (fun b n -> W.connect b n on_tile_click T.buttons_down) buttons button_names in
 
-  let board = Bogue.make connections [layout]  in
-  Bogue.run ~before_display board;;
+  let board = Bogue.make connections [layout] in
+  Bogue.run ~before_display board
 
 let () = main ();
   Bogue.quit ()
